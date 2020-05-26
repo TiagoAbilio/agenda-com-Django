7cd9afa4a08440ddac448cc8e5eb60d2faa30caa
Tiago Abilio <iagox0x0@hotmail.com>
2020-05-26 13:10:03 -0300

    Craiado a tabela de aventos para trabalho com a agenda

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from agenda.models import Eventos
 
-# Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data_event', 'data_criacao')
+    #ira chama o display no objeto criado
+    list_filter = ('data_event', 'titulo')
+
+admin.site.register(Eventos,EventoAdmin)
--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Eventos(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_event = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True) #auto_now aplica de forma auto o horario de criacao
+    # mudando nome da tabela
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    #o on_delete=models.CASCADE() dis que quando excluido o usuario ira exclir tudo que ele esta associado
+    class Meta:
+        db_table = 'Eventos'
+
+    def __str__(self):
+        return self.titulo;
